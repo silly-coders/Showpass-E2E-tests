6f6ff8474964c69ef2863eb9eb46cbefe7587864
Alex-qa-team <138615449+Alex-qa-team@users.noreply.github.com>
2023-09-14 14:34:54 -0600

    TA-72 Verify the 'Product' page content (#84)
    * Create products.cy.js
    
    * Update products.cy.js

---
+++ cypress/e2e/user-settings/products.cy.js
@@ -0,0 +1,59 @@
+describe("Verify 'Products' page by ", () => {
+  beforeEach(function () {
+    cy.clearLocalStorage();
+    cy.clearCookies();
+    cy.fixture("testdata.json").then(function (testdata) {
+      const apiRequest = "**/account/products*";
+      cy.intercept(apiRequest).as("pageLoaded");
+      this.testdata = testdata;
+      cy.navigateToHomePage();
+      cy.logIntoPortal(this.testdata.userDetails);
+      cy.clickUsernameAfterLoggingIn();
+      // Navigate to the 'Products' page
+      cy.getDropDownItem("Products").click({ force: true });
+      cy.url().should("include", "/account/products");
+      cy.wait("@pageLoaded")
+        .its("response.statusCode")
+        .should("be.oneOf", [200, 204]);
+    });
+  });
+  // ***************************************************************************
+  it(
+    "checking the 'Product' page details for consistency and correctness-TA-72",
+    { tags: ["e2e", "appearance"] },
+    function () {
+      // Page headers and sub-headers
+      cy.getH1HeaderByText("Products");
+      // Verify a list of products (product cards)
+      let listOfProducts = [
+        "Air Conditioning",
+        "Cabin Utility Pricing",
+        "Car Permit",
+        "Mission Raceway Fan Tee",
+        "Washer/Dryer Fee",
+      ];
+      for (let i = 0; i < listOfProducts.length; i++) {
+        cy.log(
+          `Going to verify the following product header: ${listOfProducts.at(
+            i
+          )}`
+        );
+        cy.get('div[id="upcoming-event-info-container"] > div > p:nth-child(1)')
+          .eq(i)
+          .should("exist")
+          .should("be.visible")
+          .should("have.text", listOfProducts.at(i));
+        // Verify a bag icon with a number 1 next to it
+        cy.get('img[data-testid="account-purchase-list-item-image"]')
+          .eq(i)
+          .should("exist")
+          .should("be.visible");
+        // Going to verify product thubmnails
+        cy.get('div[id="upcoming-event-info-container"] > div > p:nth-child(2)')
+          .eq(i)
+          .should("exist")
+          .should("be.visible");
+      }
+    }
+  );
+});
